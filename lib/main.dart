c23010aaa70e71bfab1a39e25bcffd2fcf06dc59
Kaique623 <kaiquegm623@gmail.com>
2025-02-04 14:14:22 -0300

    flutterfire configurado

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
+//Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  //Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget  {
   const MyApp({super.key});
 
-
   @override
   State<MyApp> createState() => MyAppState();
-
 }
 
 class MyAppState extends State<MyApp> {
